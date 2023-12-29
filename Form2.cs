486e48b1e45d89495683318ac31fcc3650517941
dkw05121 <dkw05121@konkuk.ac.kr>
2023-12-29 13:08:15 +0900

    Font Form 기본 UI 생성

--- Form2.cs
+++ Form2.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,21 @@ using System.Windows.Forms;
 
 namespace _01_zenix_notepad
 {
+    
     public partial class Form2 : Form
     {
         public Form2()
         {
             InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            InstalledFontCollection col = new InstalledFontCollection();
+            foreach (FontFamily fa in col.Families)
+            {
+                lbox_font.Items.Add(fa.Name);   
+            }
         }
     }
 }
